eeba49f4a71e56056a048a4c0c6fd8cee56f9753
Maximilian <maximilian_hedman@hotmail.com>
2023-05-10 22:48:26 +0200

    almost done with home section 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,48 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="index">
+    <div className="section-1">
       <div className="container">
-        <h1>WELCOME TO BMARKETO SHOP</h1>
-        <h2>Exclusive Chair</h2>
-        <h2>gold Collection.</h2>
-        <button className='btn btn-shopnow'>SHOP NOW</button>
+        <div>
+          <h1>WELCOME TO BMARKETO SHOP</h1>
+          <h2>Exclusive Chair</h2>
+          <h2>gold Collection.</h2>
+          <button className='btn btn-shopnow'>SHOP NOW</button>
+        </div>
+        <div>
+        <img src="/Placeholders/625x647.svg" alt="" />
+        </div>
       </div>
-      <img src="/Placeholders/625x647.svg" alt="" />
+
+      <div className="section-2">
+        <div className="container">
+          <h2>Best Collection</h2>
+          <ul className="shop-links">
+            <li>All&nbsp;&nbsp;/&nbsp;&nbsp;</li>
+            <li> Bags&nbsp;&nbsp;/&nbsp;&nbsp;</li>
+            <li> Dress&nbsp;&nbsp;/&nbsp;&nbsp;</li>
+            <li> Decoration&nbsp;&nbsp;/&nbsp;&nbsp;</li>
+            <li> Essentials&nbsp;&nbsp;/&nbsp;&nbsp;</li>
+            <li> Interior&nbsp;&nbsp;/&nbsp;&nbsp;</li>
+            <li> Laptop&nbsp;&nbsp;/&nbsp;&nbsp;</li>
+            <li> Mobile&nbsp;&nbsp;/&nbsp;&nbsp;</li>
+            <li> Beauty</li>
+          </ul>
+          <div className="product-container">
+            <img src="/Placeholders/270x295.svg" alt="" />
+            <img src="/Placeholders/270x295.svg" alt="" />
+            <img src="/Placeholders/270x295.svg" alt="" />
+            <img src="/Placeholders/270x295.svg" alt="" />
+          </div>
+          <div className="product-container">
+            <img src="/Placeholders/270x295.svg" alt="" />
+            <img src="/Placeholders/270x295.svg" alt="" />
+            <img src="/Placeholders/270x295.svg" alt="" />
+            <img src="/Placeholders/270x295.svg" alt="" />
+          </div>
+          <button className='btn-load'>LOAD MORE <img className='icon-sm' src="/Placeholders/rotate-cw.svg" alt="" /></button>
+        </div>
+      </div>
     </div>
   )
 }
