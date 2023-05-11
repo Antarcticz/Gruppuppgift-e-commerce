c37c63110a6706fd40df362f411cd9049ae91171
Maximilian <maximilian_hedman@hotmail.com>
2023-05-12 00:29:27 +0200

    done with 3 out of 5 sections for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,31 +19,109 @@ const Home = () => {
         <div className="container">
           <h2>Best Collection</h2>
           <ul className="shop-links">
-            <li>All&nbsp;&nbsp;/&nbsp;&nbsp;</li>
-            <li> Bags&nbsp;&nbsp;/&nbsp;&nbsp;</li>
-            <li> Dress&nbsp;&nbsp;/&nbsp;&nbsp;</li>
-            <li> Decoration&nbsp;&nbsp;/&nbsp;&nbsp;</li>
-            <li> Essentials&nbsp;&nbsp;/&nbsp;&nbsp;</li>
-            <li> Interior&nbsp;&nbsp;/&nbsp;&nbsp;</li>
-            <li> Laptop&nbsp;&nbsp;/&nbsp;&nbsp;</li>
-            <li> Mobile&nbsp;&nbsp;/&nbsp;&nbsp;</li>
+            <li>All</li>
+            <p>&#47;</p>
+            <li> Bags</li>
+            <p>&#47;</p>
+            <li> Dress</li>
+            <p>&#47;</p>
+            <li> Decoration</li>
+            <p><p>&#47;</p></p>
+            <li> Essentials</li>
+            <p><p>&#47;</p></p>
+            <li> Interior</li>
+            <p><p>&#47;</p></p>
+            <li> Laptop</li>
+            <p><p>&#47;</p></p>
+            <li> Mobile</li>
+            <p><p>&#47;</p></p>
             <li> Beauty</li>
           </ul>
           <div className="product-container">
-            <img src="/Placeholders/270x295.svg" alt="" />
-            <img src="/Placeholders/270x295.svg" alt="" />
-            <img src="/Placeholders/270x295.svg" alt="" />
-            <img src="/Placeholders/270x295.svg" alt="" />
+            <div className="card">
+              <img src="/Placeholders/270x295.svg" alt="" />
+              <p>Apple watch series</p>
+              <span>$30.00</span>
+            </div>
+            <div className="card">
+              <img src="/Placeholders/270x295.svg" alt="" />
+              <p>Table Lamp</p>
+              <span>$30.00</span>
+            </div>
+            <div className="card">
+              <img src="/Placeholders/270x295.svg" alt="" />
+              <p>laptop thinkpad lenovo</p>
+              <span>$30.00</span>
+            </div>
+            <div className="card">
+              <img src="/Placeholders/270x295.svg" alt="" />
+              <p>Table Lamp</p>
+              <span>$30.00</span>
+            </div>
           </div>
           <div className="product-container">
-            <img src="/Placeholders/270x295.svg" alt="" />
-            <img src="/Placeholders/270x295.svg" alt="" />
-            <img src="/Placeholders/270x295.svg" alt="" />
-            <img src="/Placeholders/270x295.svg" alt="" />
+            <div className="card">
+              <img src="/Placeholders/270x295.svg" alt="" />
+              <p>Gumshoes black fashion</p>
+              <span className='old-price'>$120.00</span><span>$80.00</span>
+            </div>
+            <div className="card">
+              <img src="/Placeholders/270x295.svg" alt="" />
+              <p>Woman wite dress</p>
+              <span>$30.00</span>
+            </div>
+            <div className="card">
+              <img src="/Placeholders/270x295.svg" alt="" />
+              <p>Kettle water boiler</p>
+              <span>$30.00</span>
+            </div>
+            <div className="card">
+              <img src="/Placeholders/270x295.svg" alt="" />
+              <p>Congee cooking rice cooker</p>
+              <span>$30.00</span>
+            </div>
           </div>
           <button className='btn-load'>LOAD MORE <img className='icon-sm' src="/Placeholders/rotate-cw.svg" alt="" /></button>
         </div>
       </div>
+
+      <div className="section-3">
+        <div className="container">
+          <div className="product-container">
+            <div className="card">
+              <img src="/Placeholders/369x310.svg" alt="" />
+              <p>Table Lamp - scelerisque tempore</p>
+              <span className='old-price'>$50.00</span><span>$30.00</span>
+            </div>
+            <div className="onSale-card">
+              <h1>UP TO SELL</h1>
+              <h2>50% OFF</h2>
+              <h3>Get The Best Price</h3>
+              <p>Get the best daily offer et accusam et</p>
+              <p>justo duo dolores et ea rebum. Stet</p>
+              <p>clita kasd gubergren no sea taki</p>
+              <span>Discover More</span>
+            </div>
+            <div className="card">
+              <img src="/Placeholders/369x310.svg" alt="" />
+              <p>Table Lamp - scelerisque tempore</p>
+              <span className='old-price'>$50.00</span><span>$30.00</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section-4">
+        <div className="container">
+          <div className="product-container">
+            <div className="card">
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section-5"></div>
     </div>
   )
 }
