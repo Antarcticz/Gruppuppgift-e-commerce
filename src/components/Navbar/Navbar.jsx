b8c880c0cb36b9a7810b0ece4ef194123bfc122b
Maximilian <maximilian_hedman@hotmail.com>
2023-05-15 05:15:07 +0200

    changed all css to scss and only last section on productDetails left todo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
-import './Navbar.css'
+import './Navbar.scss'
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <div className="container">
+        <div className="container-navbar">
             <Link to="/"><img src="/Placeholders/Logo.svg" alt="" /></Link>
             <ul className="nav-links">
                 <li><NavLink to="/">HOME</NavLink></li>
@@ -12,7 +12,6 @@ const Navbar = () => {
                 <li><NavLink to="/contact">CONTACT</NavLink></li>
                 <img className='icon-sm' src="/Placeholders/search.svg" alt="" />
                 <li><NavLink to="/login"><span>Login</span></NavLink></li>
-                {/* <li><NavLink to="/registration"><span>Registration</span></NavLink></li> */}
                 <img className='icon-sm' src="/Placeholders/shopping-cart.svg" alt="" />
             </ul>
         </div>
