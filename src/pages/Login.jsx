9c30d15fcfecc58fe07b07c57b36dd24a1f5d568
Israa <israahasibismail@gmail.com>
2023-05-15 13:31:54 +0200

    style login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+      <div id='login'>
+      <form action="LogIn">
+        <div>
+          <h1>Please Login to Your Account</h1>
+        </div>
+				<div>
+          <h2>E-mail*</h2><h3>Don't have an Account yet?</h3>
+					<label htmlFor="email"></label>
+					<input type="email" name="email" id="email"/> 
+				</div>
+				<div>
+          <h2>Password*</h2><h3>Forgot Your Password ?</h3>
+					<label htmlFor="passw"></label>
+					<input type="passw" name="passw" id="passw"/> 
+				</div>
+        <div>
+      <input type="checkbox" id="checkbox" name="checkbox" checked/>
+      <label for="checkbox">Please keep me logged in</label>
+      </div>
+      <div>
+        <button className='btn-submit' type='submit'>Submit</button>
+      </div>
+			</form>
+      </div>       
   )
 }
 
