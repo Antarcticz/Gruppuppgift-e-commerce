b6808d416bbd45c5302c60c1c5c299b9a63f8b21
jeslaa <jesperlaaksonen@hotmail.com>
2023-05-18 12:30:52 +0200

    Changed background color

---
+++ src/pages/Add.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addProduct } from '../store/features/products/productListSlice'
+const Add = () => {
+
+    const dispatch = useDispatch()
+    const [productData, setProductData] = useState({
+        name: '',
+        price: '',
+        imageURL: '',
+        description: ''
+    })
+
+    const handleChange = e => {
+        const { id, value } = e.target
+        setProductData(form => {
+            return {
+                ...form,
+                [id]: value
+            }
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        const data = {
+            ...productData,
+            price: +productData.price
+        }
+
+        dispatch(addProduct(data))
+    }
+}
