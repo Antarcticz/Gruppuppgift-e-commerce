d8ff9a3f6da5c8f34f0193e7a9b71fdc0de61eda
Antarcticz <114486469+Antarcticz@users.noreply.github.com>
2023-05-22 23:59:05 +0200

    Delete CandyShop directory

--- CandyShop/controllers/orderController.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const orderModel = require('../models/orderModel');
-const { verifyToken } = require('../authentication/auth')
-
-router.post('/post', verifyToken, orderModel.postOrder)
-
-router.get('/myOrders', verifyToken, orderModel.getOrders)
-
-module.exports = router;
