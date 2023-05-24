acbc3cb65af3803a1ba229018b0d7f7c609417ba
Israa <israahasibismail@gmail.com>
2023-05-24 17:21:42 +0200

    error

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import productListSlice from './features/products/productListSlice';
-import shoppingCartSlice from '../features/shoppingCart/shoppingCartSlice';
-// './features/shoppingCart./shoppingCartSlice'
-import authSlice from './features/auth/authSlice';
+import productListSlice from '../store/features/products/productListSlice';
+import shoppingCartSlice from '../store/shoppingCart/shoppingCartSlice';
 
 export const store = configureStore({
   reducer: {
     productList: productListSlice,
     shoppingCart: shoppingCartSlice,
-    auth: authSlice
   }
 })
