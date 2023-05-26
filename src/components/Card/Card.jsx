062dffae0914b0b25ba5a6420d7a0deefb4f785f
Maximilian <maximilian_hedman@hotmail.com>
2023-05-26 11:56:46 +0200

    added some style on card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const Card = ({ product }) => {
     return (
-        <Link to={`/product-details/${product.id}`}>
+        <Link to={`/product-details/${product.id}`} style={{ textDecoration: 'none' }}>
             <div className="home-product-card">
                 <img src={product.imgUrl} alt={product.productName} />
                 <p>{product.productName.slice(0, 30)}...</p>
